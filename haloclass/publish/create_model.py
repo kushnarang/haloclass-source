8613ea1db7b42926f4386ad6ac525409ceea8d44
Kush Narang <30564993+kushnarang@users.noreply.github.com>
2024-09-01 04:04:29 -0700

    Update create_model.py

--- haloclass/publish/create_model.py
+++ haloclass/publish/create_model.py
@@ -9,7 +9,7 @@ from haloclass.publish.helpers import load_pickled_dataset
 class CreateModelArgs(Tap):
     data_path: str = "publication-datasets"
     embeddings_source: str = "train.150.embeddings"
-    label_source: str = "train.150.labels"
+    labels_source: str = "train.150.labels"
     save_name: str = 'publication-datasets/final-haloclass-model.pkl'
 
 
@@ -23,7 +23,7 @@ PARAMS = {
 def main():
     args = CreateModelArgs().parse_args()
     data_path = Path(args.data_path)
-    training_x, training_y = load_pickled_dataset(data_path / args.embeddings_source, data_path / args.label_source)
+    training_x, training_y = load_pickled_dataset(data_path / args.embeddings_source, data_path / args.labels_source)
 
     model = svm.SVC(probability=True, **PARAMS)
     model.fit(training_x, training_y)
